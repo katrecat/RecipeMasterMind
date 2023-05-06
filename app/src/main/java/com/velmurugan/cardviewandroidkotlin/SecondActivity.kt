ce5c38505721973c28c541d26aa78235ef7acfbd
Katsiaryna Herasimovich <katrecat33@gmail.com>
2023-05-06 16:28:58 +0200

    [#4] Fixing scrolling error

--- app/src/main/java/com/velmurugan/cardviewandroidkotlin/SecondActivity.kt
+++ app/src/main/java/com/velmurugan/cardviewandroidkotlin/SecondActivity.kt
@@ -1,19 +1,22 @@
 package com.velmurugan.cardviewandroidkotlin
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.ListView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.recipemastermind.R
 
 class SecondActivity : AppCompatActivity() {
     private lateinit var recipeNameTextView: TextView
     private lateinit var recipeImageView: ImageView
-    private lateinit var ingredientsListView: ListView
-    private lateinit var stepsListView: ListView
+    private lateinit var ingredientsRecyclerView: RecyclerView
+    private lateinit var stepsRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,19 +32,67 @@ class SecondActivity : AppCompatActivity() {
         recipeNameTextView = findViewById(R.id.text_recipe_name)
         recipeNameTextView.text = recipeName
         recipeImageView = findViewById(R.id.image_recipe)
-        ingredientsListView = findViewById(R.id.listview_ingredients)
-        stepsListView = findViewById(R.id.listview_steps)
+        ingredientsRecyclerView = findViewById(R.id.recyclerview_ingredients)
+        stepsRecyclerView = findViewById(R.id.recyclerview_steps)
 
         Glide.with(this)
             .load(imageURL)
             .centerCrop()
             .into(recipeImageView)
 
-        val ingredientsAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, ingredientsList)
-        ingredientsListView.adapter = ingredientsAdapter
+        ingredientsRecyclerView.layoutManager = LinearLayoutManager(this)
+        stepsRecyclerView.layoutManager = LinearLayoutManager(this)
 
+        val ingredientsAdapter = IngredientsAdapter(ingredientsList)
+        ingredientsRecyclerView.adapter = ingredientsAdapter
 
-        val stepsAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, stepsList)
-        stepsListView.adapter = stepsAdapter
+        val stepsAdapter = StepsAdapter(stepsList)
+        stepsRecyclerView.adapter = stepsAdapter
+    }
+
+    private inner class IngredientsAdapter(private val ingredientsList: List<String>) :
+        RecyclerView.Adapter<IngredientsAdapter.ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(android.R.layout.simple_list_item_1, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val ingredient = ingredientsList[position]
+            holder.textView.text = ingredient
+        }
+
+        override fun getItemCount(): Int {
+            return ingredientsList.size
+        }
+
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val textView: TextView = itemView.findViewById(android.R.id.text1)
+        }
+    }
+
+    private inner class StepsAdapter(private val stepsList: List<String>) :
+        RecyclerView.Adapter<StepsAdapter.ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(android.R.layout.simple_list_item_1, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val step = stepsList[position]
+            holder.textView.text = step
+        }
+
+        override fun getItemCount(): Int {
+            return stepsList.size
+        }
+
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val textView: TextView = itemView.findViewById(android.R.id.text1)
+        }
     }
 }
